59cb8cdd929a9e006bd9a69e88fae24c465beec1
Samuel Annan <samuelannan191@gmail.com>
2024-05-07 14:19:25 -0700

    javaScript

---
+++ index.js
@@ -0,0 +1,19 @@
+ // comment 
+ console.log("Hello World!!!");
+
+ //there are issues with var
+ let firstName = "Samuel";
+ console.log(firstName);
+ //variable name cannot contain space or hyphen
+
+ //Declaring multiple variables
+
+ let firstName2,lastName;
+ firstName2= "reference";
+ lastName= " Annan";
+console.log(firstName + lastName);
+
+//Constants
+const interestRate = 0.7;
+
+console.log(interestRate);
